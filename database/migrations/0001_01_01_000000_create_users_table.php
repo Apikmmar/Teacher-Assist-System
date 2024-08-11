035a677cd6f2060bec93411aebf41fb26b75d3cc
apikammar <apikammar07@gmail.com>
2024-08-12 01:20:18 +0800

    migrate db

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,16 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('teacher_id', 20);
+            $table->string('name', 100);
+            $table->string('ic', 12);
+            $table->string('gender', 10);
+            $table->string('contact', 15);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('verification');
+            $table->string('photo')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
---
+++ database/migrations/2024_08_11_171534_create_students_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('classroom_id')->constrained()->onDelete('cascade');
+            $table->string('student_id', 20);
+            $table->string('name', 100);
+            $table->string('ic', 12);
+            $table->string('gender', 10);
+            $table->date('dob');
+            $table->date('join_school_date');
+            $table->string('status', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
