05f61e52a06afbf073e21188d965f9127e7aa35e
apikammar <apikammar07@gmail.com>
2024-08-14 16:45:09 +0800

    update migration

--- database/migrations/2024_08_14_084229_create_role__users_table.php
+++ database/migrations/2024_08_14_084229_create_role__users_table.php
@@ -11,10 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('roles', function (Blueprint $table) {
+        Schema::create('role__users', function (Blueprint $table) {
             $table->id();
-            $table->enum( 'name', ['Class Teacher', 'Management', 'Coordinator']);
-            $table->string('role_description', 100);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -24,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('roles');
+        Schema::dropIfExists('role__users');
     }
 };
