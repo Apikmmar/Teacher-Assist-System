20f1aed90c99b4e758dfb6e6a9900cad1005c25d
apikammar <apikammar07@gmail.com>
2024-09-20 15:58:47 +0800

    update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Arr;
 
 class UserSeeder extends Seeder
 {
@@ -26,7 +28,23 @@ class UserSeeder extends Seeder
             ['teacher_id' => 'TC77777', 'name' => 'lily', 'ic' => '777777777777', 'gender' => 'Women', 'contact' => '1234567890', 'email' => 'lily@gmail.com', 'password' => Hash::make('777777777777'), 'verification' => 'teacher_verification.pdf', 'photo' => NULL, 'created_at' => $now, 'updated_at' => $now],
             ['teacher_id' => 'TC88888', 'name' => 'rose', 'ic' => '888888888888', 'gender' => 'Women', 'contact' => '1234567890', 'email' => 'rose@gmail.com', 'password' => Hash::make('888888888888'), 'verification' => 'teacher_verification.pdf', 'photo' => NULL, 'created_at' => $now, 'updated_at' => $now],
             ['teacher_id' => 'TC99999', 'name' => 'daisy', 'ic' => '999999999999', 'gender' => 'Women', 'contact' => '1234567890', 'email' => 'daisy@gmail.com', 'password' => Hash::make('999999999999'), 'verification' => 'teacher_verification.pdf', 'photo' => NULL, 'created_at' => $now, 'updated_at' => $now],
-            
         ]);
+
+        for ($i=0; $i < 30; $i++) {
+            $randomDOB = Carbon::create(rand(1974, 2000), rand(1, 12), rand(1, 28));
+            $randomTCID = 'TC' . rand(11111, 99999);
+            $icFormat = $randomDOB->format('ymd') . str_pad(rand(1, 12), 2, '0', STR_PAD_LEFT) . str_pad(rand(0, 9999), 4, '0', STR_PAD_LEFT);
+            $randName = Arr::random(['Kamal', 'Ariff', 'Abby', 'Amir', 'Chua', 'Naga', 'Zimi', 'Ieka']);
+            $randGender = Arr::random(['Men', 'Women']);
+
+            do {
+                $randEmail = $randName . rand(10, 99) . '@gmail.com';
+                $emailExists = DB::table('users')->where('email', $randEmail)->exists();
+            } while ($emailExists);
+
+            DB::table('users')->insert([
+                ['teacher_id' => $randomTCID, 'name' => $randName, 'ic' => $icFormat, 'gender' => $randGender, 'contact' => '1234567890', 'email' => $randEmail, 'password' => Hash::make('12345678'), 'verification' => 'teacher_verification.pdf', 'photo' => NULL, 'created_at' => $now, 'updated_at' => $now],
+            ]);
+        }
     }
 }
